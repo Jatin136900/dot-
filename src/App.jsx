e4bd05527847b9075d9f53da4ecaa26e7b91f0fe
Jatin <jatinverma2428@gmail.com>
2025-09-30 16:37:56 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,59 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [array, setArray] = useState([]);
-  const [redo, setRedo] = useState([]);
-  const [btn, setBtn] = useState(true);
 
+  const [circles, SetCircles] = useState([]);
+  const [deletedCircles, SetDeletedCircles] = useState([]);
 
-  const handleClick = (e) => {
+  function handelclick(e) {
+    if (e.target.nodeName === "BUTTON") return;
     const obj = {
       id: Date.now(),
       x: e.clientX,
       y: e.clientY,
-    };
-    setArray([...array, obj]);
-    setRedo([]);
-  };
+    }
 
-  const handleReset = (e) => {
-    e.stopPropagation();
-    setArray([]);
-    setRedo([]);
-    setBtn(true);
-  };
+    // push obj in circle 
+    SetCircles([...circles, obj]); // circle.push(obj)
+  }
 
+  function handleUndo() {
+    let copy = circles;
+    const lastInsertedCircle = copy.pop();
+    SetDeletedCircles([...deletedCircles, lastInsertedCircle]);
+  }
 
-  const handleUndo = (e) => {
-    e.stopPropagation();
-    if (array.length === 0) return;
-    const newArray = [...array];
-    const removed = newArray.pop();
-    setArray(newArray);
-    setRedo([removed, ...redo]);
-  };
+  function handleRedo() {
+    let copy = circles;
+    const lastDeletedCircle = copy.pop();
+    SetCircles([...circles, lastDeletedCircle]);
+  }
 
-
-  const handleRedo = (e) => {
-    e.stopPropagation();
-    if (redo.length === 0) return;
-    const newRedo = [...redo];
-    const restored = newRedo.shift();
-    setRedo(newRedo);
-    setArray([...array, restored]);
-  };
-
-
-  useEffect(() => {
-    setBtn(array.length === 0);
-  }, [array]);
-
-  return (
-    <>
-      <div id="wrapper" onClick={handleClick}>
-        <div id="buttons" >
-          <button className="btn" onClick={handleReset} disabled={btn}> Reset </button>
-          <button className="btn" onClick={handleUndo} disabled={btn}> Undo </button>
-          <button className="btn" onClick={handleRedo} disabled={redo.length === 0}> Redo </button>
-        </div>
-
-        {array.map((item) => (
-          <div key={item.id} className="circle" style={{ position: "absolute", top: item.y - 25, left: item.x - 25, pointerEvents: "none", }}></div>
-        ))}
+  console.log("circles" + circles);
+  console.log("deleted" + deletedCircles)
+  return <>
+    <div className="wrapper" onClick={handelclick}>
+      <div className="buttons">
+        <button>Reset</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleRedo}>Redo</button>
       </div>
-    </>
-  );
+
+      {
+        circles.length > 0 ?
+          circles.map((obj) => {
+            return (
+              <div className="circle" style={{ top: obj.y - 7.5 + "px", left: obj.x - 7.5 + "px" }}></div>
+            );
+          })
+          : ""
+      }
+
+    </div>
+
+  </>
+
 }
 
 export default App;
